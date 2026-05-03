fd082444fb942a881886f825e18bb565d127bef6
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-05-03 14:39:13 +0530

    Add files via upload

---
+++ L3A1.py
@@ -0,0 +1,27 @@
+# 1. if and else statments
+# age = int(input("enter your age:"))
+# if age >= 18:
+#     print("you can vote")
+# else:
+#     print("you cannot vote")
+
+# 2. nested if and else 
+# num = int(input("enter a number:"))
+# if num >= 50:
+#     if num % 2 == 0:
+#         print("number is greater then 50 and even")
+#     else:
+#         print ("number is greater than 50 and odd")
+# else:
+#     print("number is smaller than 50")
+        
+# 3. if elif else
+marks = int(input( "enter your marks" ))
+if marks >= 95:
+    print("A")
+elif marks >= 85:
+    print("B")
+else:
+    print("D")
+
+
---
+++ L3A2.py
@@ -0,0 +1,14 @@
+marks = int(input("enter your marks"))
+if marks >= 95:
+    print("A+")
+elif marks >= 90:
+    print("A")
+elif marks >= 85:
+    print("B+")
+elif marks >= 80:
+    print("B")
+elif marks >= 70:
+    print("c")
+else:
+    marks <= 60
+    print("FAIL")
