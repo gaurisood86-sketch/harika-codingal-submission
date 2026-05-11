5dd226ad1b61aaea78c7dc0a29e6e9768bad9ca6
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-05-11 15:36:14 +0530

    Add files via upload

---
+++ L2A2.py
@@ -0,0 +1,14 @@
+
+number1 = input("Enter Value of number1:")
+number2 = input("Enter Value of number2:")
+number3 = input("Enter Value of number3:")
+pen=number1
+pencil=number3
+number1 = number2
+number2 = pencil
+number3 = pen
+
+
+print("value of number1 after swapping", number1)
+print("value of number2 after swapping", number2)
+print("value of number3 after swapping", number3)
