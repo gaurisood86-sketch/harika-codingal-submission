e6fd7ef769994784ea6f7e399a372e95fb85955a
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-05-11 15:47:36 +0530

    Add files via upload

---
+++ L4A2.py
@@ -0,0 +1,24 @@
+
+for i in range(1, 11):
+    print(f"23 x {i} = {23 * i}")
+
+
+total_sum = 0
+num = 1
+while num <= 10:
+    total_sum += num
+    num += 1
+print(f"The sum of the first ten natural numbers is {total_sum}")
+
+
+
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is not a prime number.")
+            break
+    else:
+        print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
