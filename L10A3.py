bc6dac22249689d6ac86177262acfe8a8de9d069
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-05-15 11:42:01 +0530

    Add files via upload

---
+++ L10A3.py
@@ -0,0 +1,6 @@
+
+user_data_tuple = ("Harika", "Trehan", "height: 155 cm", "weight:32", "age:12" , "Mathematics")
+user_data_list = list(user_data_tuple)
+print("Original Tuple:", user_data_tuple)
+print("Converted List:", user_data_list)
+print("Data Type:", type(user_data_list))
