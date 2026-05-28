41a8c13add2aa84f32a07fd2d83b637eac7b6e44
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-05-28 16:41:32 +0530

    Add files via upload

--- L3A1.py
+++ L3A1.py
@@ -1,27 +1,13 @@
-# 1. if and else statments
-# age = int(input("enter your age:"))
-# if age >= 18:
-#     print("you can vote")
-# else:
-#     print("you cannot vote")
+with open("sample.txt" , "w") as file:
+    file.write("hello this is a test file")
+    file.write("python is very fun to learn")
+    print("data was given sucessfully")
 
-# 2. nested if and else 
-# num = int(input("enter a number:"))
-# if num >= 50:
-#     if num % 2 == 0:
-#         print("number is greater then 50 and even")
-#     else:
-#         print ("number is greater than 50 and odd")
-# else:
-#     print("number is smaller than 50")
-        
-# 3. if elif else
-marks = int(input( "enter your marks" ))
-if marks >= 95:
-    print("A")
-elif marks >= 85:
-    print("B")
-else:
-    print("D")
+text="python is very fun to learn "
+with open ("words.txt" , "w") as file:
+    sample=text.split()
+    for i in sample:
+        file.write(i+"\n")
+print("the data was given sucessfully")
 
 
--- L3A2.py
+++ L3A2.py
@@ -1,14 +1,38 @@
-marks = int(input("enter your marks"))
-if marks >= 95:
-    print("A+")
-elif marks >= 90:
-    print("A")
-elif marks >= 85:
-    print("B+")
-elif marks >= 80:
-    print("B")
-elif marks >= 70:
-    print("c")
+import os
+try:
+    with open ("newfile.txt" , "x") as file:
+        file.write("this is a newly created file")
+    print("the was created sucessfully")
+except FileExistsError:
+    print("this file already exists")
+
+filename="sample.txt"
+if os.path.exists(filename):
+    print("this file exists")
 else:
-    marks <= 60
-    print("FAIL")
+    print("this file does not exist")
+
+
+
+filename="data.txt"
+if not os.path.exists(filename):
+    with open ("data.txt" , "w") as file:
+     file.write("this is a newly created file")
+    print("the file has been created")
+else:
+    print("this file alreadly exists")
+
+filename="sample.txt"
+if os.path.exists(filename):
+    os.remove(filename)
+    print(f"the {filename} has been deleted sucessfully")
+else:
+    print(f"file {filename} does not exist")
+
+
+folder_name="testfolder"
+if os.path.exists(folder_name):
+    os.rmdir(folder_name)
+    print(f"the {folder_name} has been deleted sucessfully")
+else:
+    print(f" this folder {folder_name} does not exist")    
