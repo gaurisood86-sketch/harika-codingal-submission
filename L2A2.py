a75276994c9f0d6f77751f58c10e1a8e5790fe94
gaurisood86-sketch <gaurisood.86@gmail.com>
2026-06-03 16:12:48 +0530

    Add files via upload

--- L2A2.py
+++ L2A2.py
@@ -1,12 +1,60 @@
-with open ("sample.txt" , "r") as file:
-    print("reading part of the line")
-    print(file.read(10))
+import pygame
 
-with open ("sample.txt" , "r") as file:
-    print("reading one single line")
-    print(file.readline())
+def main():
+    pygame.init()
+    screen_width,screen_height=500,500
+    screen=pygame.display.set_mode((screen_width, screen_height))
+    screen=pygame.display.set_caption("changing the color ! ")
 
-with open ("sample.txt" , "r") as file:
-    print("looping through file lines")
-    for line in file:
-        print(line.strip())
+    colors={
+     'red':pygame.Color('red'),
+     'green':pygame.Color('green'),
+     'yellow':pygame.Color('yellow'),
+     'blue':pygame.Color('blue'),
+     'white':pygame.Color('white')
+    }
+    current_color=colors['white']
+
+    x,y=30,30
+    sprite_width,sprite_height=60,60
+
+    clock=pygame.time.Clock()
+
+    done=False
+    while not done:
+        for event in pygame.event.get():
+             if event.type==pygame.QUIT:
+              done=True
+
+        pressed=pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]: x-=3
+        if pressed[pygame.K_RIGHT]: x+=3
+        if pressed[pygame.K_UP]: y-=3
+        if pressed[pygame.K_DOWN]: y+=3
+
+        x=min(max(0,x), screen_width - sprite_width)
+        y=min(max(0,y), screen_height-sprite_height)
+
+        if x ==0: current_color=colors['blue']
+        elif x ==screen_width-sprite_width: current_color=colors['yellow']
+        elif y ==0: current_color=colors['red']
+        elif y ==screen_height-sprite_height: current_color=colors['green']
+
+        else:
+         current_color=colors['white']
+
+
+        screen.fill((255,255,255))
+
+        pygame.draw.rect(screen, current_color,
+                         (x, y, sprite_width, sprite_height))
+        pygame.display.flip()
+        clock.tick(90)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
+
+
